3c2f611c465e2f58bb50db8291c7b5b60ec2696c
Ashish Mishra <ashishm598@gmail.com>
2018-06-21 17:54:21 +0530

    VersionName : 1.0.1 VersionCode : 2

--- app/src/main/java/com/liveinpride/android/ui/home/core/HomeModel.java
+++ app/src/main/java/com/liveinpride/android/ui/home/core/HomeModel.java
@@ -2,10 +2,9 @@ package com.liveinpride.android.ui.home.core;
 
 public class HomeModel {
 
-    public final String URL = "https://www.liveinpride.app/?utm_source=APP&utm_medium=WEBSITE&utm_campaign=Android";
+    private final String URL = "https://www.liveinpride.app/?utm_source=APP&utm_medium=WEBSITE&utm_campaign=Android";
 
-
-    public String getWebURL(){
+    public String getWebURL() {
         return URL;
     }
 
